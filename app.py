0b2a098c8ecf5d34374220b2ca909f6e66517516
Sammi Rosser <samanthajaderosser@gmail.com>
2024-12-04 17:48:15 +0000

    Add option to submit more structured log

--- app.py
+++ app.py
@@ -145,6 +145,8 @@ st.session_state.submitter_name = st.text_input(
             "**What's your name?**\n\n*Please include your first name and surname*"
         )
 
+st.write("*Choose between 'Quick' for a simple one-box project log template, or 'Structured' if you'd like some more guidance on what to include in your project update*")
+
 project_form_simple, project_form_structured = st.tabs(["Quick", "Structured"])
 
 with project_form_simple:
@@ -204,5 +206,155 @@ with project_form_simple:
         message
 
 
+
+
+def run_structured_submit():
+    # key_progress_log, key_meetings_log, additional_notes_log
+    # challenges_log, key_planned_activities_log, other_comments_log
+    print(f"Project Code: {st.session_state.project_code}")
+    print(f"Submitter: {st.session_state.submitter_name}")
+
+    if st.session_state.project_code is None:
+        message = st.warning("Please select a project before submitting")
+    elif st.session_state.submitter_name == "":
+        message = st.warning("Please enter your name before submitting")
+    elif (st.session_state.key_progress_log == "" and
+          st.session_state.key_meetings_log == "" and
+          st.session_state.additional_notes_log == "" and
+          st.session_state.challenges_log == "" and
+          st.session_state.key_planned_activities_log == "" and
+          st.session_state.other_comments_log == ""):
+        message = st.warning("Please enter an update in at least one box before submitting")
+    else:
+        rows = run_query_main()
+
+        with st.spinner("Submitting Log..."):
+
+            id_of_latest_entry = rows.data[(len(rows.data) - 1)]['id']
+            id_for_entry = id_of_latest_entry + 1
+            print(f"ID of latest entry: {id_of_latest_entry}")
+
+            structured_log_dict = [
+                {"entry_type": "Structured Log - Progress", "entry": st.session_state.key_progress_log},
+                {"entry_type": "Structured Log - Meetings", "entry": st.session_state.key_meetings_log},
+                {"entry_type": "Structured Log - Additional Progress Notes", "entry": st.session_state.additional_notes_log},
+                {"entry_type": "Structured Log - Challenges", "entry": st.session_state.challenges_log},
+                {"entry_type": "Structured Log - Planned Activities", "entry": st.session_state.key_planned_activities_log},
+                {"entry_type": "Structured Log - Other Comments", "entry": st.session_state.other_comments_log},
+            ]
+
+            for box in structured_log_dict:
+                entry_dict = {
+                            "id":id_for_entry,
+                            "created_at": datetime.now(timezone.utc).isoformat(),
+                            "project_code": int(st.session_state.project_code), # Ensure not passing as int64, which table will reject
+                            "submitter": st.session_state.submitter_name,
+                            "entry_type": box["entry_type"],
+                            "entry": box["entry"]
+                        }
+
+                try:
+                    print(f"Attempting to write as entry {id_for_entry}")
+                    response = (
+                        supabase.table("ProjectLogs").insert(entry_dict).execute()
+                    )
+                    if response.data:
+                        print("Successfully written to ProjectLogs table on first try")
+                        message = st.success(f"""
+                                            Project Log Submitted Successfully!
+                                            \n\n**Project**: {st.session_state.project_code}
+                                            \n\n**Submitter**: {st.session_state.submitter_name}
+                                            \n\n**Log**: {box["entry"]}
+                                            """)
+                        celebrate()
+                    else:
+                        raise Exception(response.error.message)
+                except Exception as e:
+                    error_message = str(e)
+                    print(f"Error occurred: {error_message}")
+                    # print("Error encountered while trying to write to ProjectLogs table.")
+                    print("Trying again...")
+                    i = 1
+                    while i<=10:
+                        try:
+                            entry_dict['id'] += 1
+                            print(f"Retrying with id {entry_dict['id']} - retry {i}")
+                            response = (
+                                supabase.table("ProjectLogs").insert(entry_dict).execute()
+                            )
+                            if response.data:
+                                print(f"Successfully written on retry {i}")
+                                message = st.success(f"""
+                                            Project Log Submitted Successfully!
+                                            \n\n**Project**: {st.session_state.project_code}
+                                            \n\n**Submitter**: {st.session_state.submitter_name}
+                                            \n\n**Log**: {st.session_state.project_update}
+                                            """)
+                                celebrate()
+                            else:
+                                raise Exception(response.error.message)
+                            break
+                        except Exception as e:
+                            error_message = str(e)
+                            print(f"Error occurred: {error_message}")
+                            i += 1
+                            sleep(2)
+                        message = st.warning("Error Submitting Log - Please Contact Dan or Sammi on Slack")
+
+
 with project_form_structured:
-    st.write("Coming Soon!")
+    key_progress, bs1, key_meetings, bs2, additional_notes = st.columns([0.3,0.05,0.3,0.05,0.3])
+
+    key_progress.write("#### Project Progress")
+
+    st.session_state.key_progress_log = key_progress.text_area(
+        "Please enter what progress you have made with your project since your last update",
+        height=250
+    )
+
+    key_meetings.write("#### Project-related Meetings")
+    st.session_state.key_meetings_log = key_meetings.text_area(
+        "Provide a brief overview of any meetings you have had with stakeholders or other parties since your last update",
+        height=250
+    )
+
+    additional_notes.write("#### Additional Notes on Progress")
+    st.session_state.additional_notes_log = additional_notes.text_area(
+        "Use this space for any additional notes about your project progress so far that don't fit into the previous categories",
+        height=250
+    )
+
+    st.subheader("Challenges and Next Steps")
+
+    challenges, bs3, key_planned_activities, bs4, other_comments = st.columns([0.3,0.05,0.3,0.05,0.3])
+
+    challenges.write("#### Challenges")
+    st.session_state.challenges_log = challenges.text_area(
+        """What challenges have you faced in your project since your last update?
+        \n\nDo you have any updates on previous challenges you have faced?
+        \n\nIf you have any blockers that you need the HSMA team's input on, please contact us on Slack.
+        """,
+        height=250
+    )
+
+    key_planned_activities.write("#### Next Steps")
+    st.session_state.key_planned_activities_log = key_planned_activities.text_area(
+        """What are you planning to do in the next month?
+        \n\nWhat are your key next steps?
+        """,
+        height=250
+    )
+
+    other_comments.write("#### Any Other Comments")
+
+    st.session_state.other_comments_log = other_comments.text_area(
+    """Use this space for any other comments that don't fit under any of the other headers
+    """,
+        height=250
+    )
+
+    submit_structured_project_log = st.button("Submit Update", key="submit_update_structured",
+                                              type='primary', disabled=False,
+                                              on_click=run_structured_submit)
+
+    message
